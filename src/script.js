6b051015bde16bac16055252fe88320ae211f1a6
Ali Hassan <hassanali8812@gmail.com>
2024-09-12 16:54:07 +0500

    bug fixed

--- src/script.js
+++ src/script.js
@@ -17,12 +17,12 @@ lotteryTimes.forEach(time => {
         <div class="flex max-lg:flex-col justify-between items-center mb-2">
             <h2 class="text-lg font-semibold">Anguilla ${time}</h2>
             <div class="flex gap-4 items-center">
-                <span class="flex gap-1 h-5 items-center text-gray-500"><img src="../assets/calendar.svg" alt="calendar">Dom 08 de septiembre, 2024</span>
-                <span class="flex gap-1 h-5 items-center text-right text-[#005643] font-black"><img src="../assets/history.svg" alt="history">${time === '9AM' ? '9:00AM' : time}</span>
+                <span class="flex gap-1 h-5 items-center text-gray-500"><img src="assets/calendar.svg" alt="calendar">Dom 08 de septiembre, 2024</span>
+                <span class="flex gap-1 h-5 items-center text-right text-[#005643] font-black"><img src="assets/history.svg" alt="history">${time === '9AM' ? '9:00AM' : time}</span>
             </div>
         </div>
         <div class="flex max-lg:flex-col items-center justify-between">
-            <img src="../assets/logo2.svg" alt="Anguilla Lottery" class="mr-4">
+            <img src="assets/logo2.svg" alt="Anguilla Lottery" class="mr-4">
             <div class="flex space-x-2">
                 ${Array.from({length: 3}, () => `
                     <div class="w-20 h-20 text-3xl rounded-full bg-teal-100 flex items-center justify-center text-teal-700 font-bold">
